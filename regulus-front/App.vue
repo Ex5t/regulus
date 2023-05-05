6ea513408f1487a5390f5e0ddc37abf417befab5
Stefan Sabo <sabo.stefa@gmail.com>
2023-05-05 16:04:20 +0200

    Added untracked files.

---
+++ regulus-front/App.vue
@@ -0,0 +1,71 @@
+<template>
+  <head>
+  </head>
+
+  <body >
+    <div>  <Nav/></div>
+
+    <p><br></p>
+    <p><br></p>
+    <h1>NASLOV</h1>
+    <p><br></p>
+    <p><br></p>
+    <h1>NASLOV</h1>
+    <p><br></p>
+    <p><br></p>
+    <h1>NASLOV</h1>
+    <p><br></p>
+    <p><br></p>
+    <h1>NASLOV</h1>
+    <p><br></p>
+    <p><br></p>
+    <h1>NASLOV</h1>
+    <p><br></p>
+    <p><br></p>
+    <h1>NASLOV</h1>
+    <p><br></p>
+    <p><br></p>
+    <h1>NASLOV</h1>
+    <p><br></p>
+    <p><br></p>
+    <h1>NASLOV</h1>
+    <p><br></p>
+    <p><br></p>
+    <h1>NASLOV</h1>
+    <p><br></p>
+    <p><br></p>
+    <h1>NASLOV</h1>
+    <p><br></p>
+    <p><br></p>
+    <h1>NASLOV</h1>
+    <p><br></p>
+    <p><br></p>
+    <h1>NASLOV</h1>
+    <p><br></p>
+    <p><br></p>
+    <h1>NASLOV</h1>
+    <p><br></p>
+    <p><br></p>
+    <h1>NASLOV</h1>
+    
+
+  </body>
+  <!-- <Nav/> -->
+  
+  
+  
+  
+</template>
+
+<style>
+h1{
+  color: red;
+}
+
+  body{
+    background-size:100% ;
+    background-image: url("/static/bg_stain.png");
+    /* background-color: red; */
+  }
+  
+</style>
